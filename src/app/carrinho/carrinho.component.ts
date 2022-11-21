29ac0091c6e2f14b81d44718554d88e5d4babaf2
francivaldolima <francivaldolimadesousa@gmail.com>
2022-11-21 10:53:00 -0300

    adicionada página de contato

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,8 @@
+
+import { CarrinhoService } from './../carrinho.service';
 import { Component, OnInit } from '@angular/core';
+import { IProdutoCarrinho } from '../produtos';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total =0;
 
-  constructor() { }
+  constructor(
+    public carrinhoService:CarrinhoService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obterCarrinho();
+    this.calcularTotal();
+  }
+
+  calcularTotal(){
+    this.total = this.itensCarrinho.reduce((prev, curr)=> prev + (curr.preco * curr.quantidade), 0)
+  }
+
+  removerProdutoCarrinho(produtoId: number){
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
+  }
+
+  comprar(){
+    alert("Parabéns, você finalizou a sua comprar!");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
+
   }
 
 }
